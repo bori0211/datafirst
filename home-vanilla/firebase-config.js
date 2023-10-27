c8d2c2af6b299c421f86403181bb37740e7a7871
김석태 (datafirst-ec2) <bori0211@gmail.com>
2023-10-27 21:47:18 +0900

    firebase-config

---
+++ home-vanilla/firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCyzzvT6e1aST6P22YefJ73Tyj5TQbzCvU",
+    authDomain: "home-firebase-2a3f6.firebaseapp.com",
+    databaseURL: "https://home-firebase-2a3f6.firebaseio.com",
+    projectId: "home-firebase-2a3f6",
+    storageBucket: "home-firebase-2a3f6.appspot.com",
+    messagingSenderId: "3947199852",
+    appId: "1:3947199852:web:0c4c6e3801e8a2ae990749"
+};
