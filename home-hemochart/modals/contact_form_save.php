8e571768de3d886fcee20fefd582cc53749f8af7
김석태 (datafirst-ec2) <bori0211@gmail.com>
2025-09-04 15:32:25 +0900

    gmail pass

--- home-hemochart/modals/contact_form_save.php
+++ home-hemochart/modals/contact_form_save.php
@@ -39,7 +39,7 @@
 		$mail->Host       = "smtp.gmail.com";                     //Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                 //Enable SMTP authentication
 		$mail->Username   = "datafirst.contact@gmail.com";        //SMTP username
-		$mail->Password   = "qkvycmlftbfiwndf";                   //SMTP password
+		$mail->Password   = GMAIL_DATAFIRST_PASSWORD;             //SMTP password
 		//$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;        //Enable implicit TLS encryption
 		$mail->SMTPSecure = "tls";
 		$mail->Port       = 587;                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
